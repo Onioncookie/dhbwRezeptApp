f530143f81a1961c1be0b6fd6f70c391ea1fb234
Onioncookie <markgilgen90@gmail.com>
2019-06-30 13:57:07 +0200

    - Added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { RecipeCreateComponent } from './components/recipe-create/recipe-create.component';
+import { IngredientCreateComponent } from './components/ingredient-create/ingredient-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingCartComponent,
+    RecipeCreateComponent,
+    IngredientCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
